10ea30a84485115580bf0dc4e1913820b0d4374a
hdvsdaejv66-sudo <hdvsdaejv66@gmail.com>
2026-03-20 22:22:37 +0900

    CRUD update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/layout/Sidebar";
 import TopBar from "@/components/layout/TopBar";
+import Providers from "@/components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({
         <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <TopBar />
           <main className="flex-1 overflow-y-auto p-6">
-            {children}
+            <Providers>
+              {children}
+            </Providers>
           </main>
         </div>
       </body>
